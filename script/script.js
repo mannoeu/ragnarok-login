9b388013ca06430afca167dbca0a727573489f80
MANN03U <emmanuel_costasilva@hotmail.com>
2020-02-02 16:40:04 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+﻿var modal = document.querySelector('.modal');
+
+var login = document.querySelector('#login').addEventListener('click', function (e) {
+  e.preventDefault();
+  modal.classList.toggle('active');
+});
+
+var closeModal = document.querySelector('#closeModal').addEventListener('click', function (e) {
+  e.preventDefault();
+  modal.classList.toggle('active');
+});
+
+var exit = document.querySelector('#exit').addEventListener('click', function (e){
+  e.preventDefault();
+  alert('Até breve');
+})
