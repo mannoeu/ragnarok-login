ac9095cc7e690c59de04a9538294cc350ade9d63
MANN03U <emmanuel_costasilva@hotmail.com>
2020-04-17 22:18:04 -0300

    add loading screen and progress bar

--- script/script.js
+++ script/script.js
@@ -1,16 +1,42 @@
-﻿var modal = document.querySelector('.modal');
+﻿const modal = document.querySelector(".modal");
+const modalLoading = document.querySelector(".modal-loading");
 
-var login = document.querySelector('#login').addEventListener('click', function (e) {
-  e.preventDefault();
-  modal.classList.toggle('active');
-});
+const user = document.querySelector("#user");
+const password = document.querySelector("#password");
 
-var closeModal = document.querySelector('#closeModal').addEventListener('click', function (e) {
-  e.preventDefault();
-  modal.classList.toggle('active');
-});
+const login = document
+  .querySelector("#login")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    if (user.value && password.value) {
+      modal.classList.toggle("active");
+    } else {
+      alert("Preencha os campos");
+    }
+  });
 
-var exit = document.querySelector('#exit').addEventListener('click', function (e){
-  e.preventDefault();
-  alert('Até breve');
-})
+const closeModal = document
+  .querySelector("#closeModal")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.classList.toggle("active");
+  });
+
+const exit = document
+  .querySelector("#exit")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Até breve");
+  });
+
+const initLoading = document
+  .querySelector("#initLoading")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+    modalLoading.classList.toggle("active");
+
+    setTimeout(() => {
+      modalLoading.classList.toggle("active");
+      modal.classList.toggle("active");
+    }, 6000);
+  });
